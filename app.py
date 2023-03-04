d5ec38f73fd43d91355a04a18f5c2b42e805316c
Juan <juanxxrvillanueva@outlook.com>
2023-03-04 12:31:56 -0600

    clase bd fix 04/03/2023

--- app.py
+++ app.py
@@ -10,18 +10,24 @@ app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 csrf = CSRFProtect(app)
 
-@app.get("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-  return render_template("index.j2")
-
-@app.post("/")
-def index_post():
   create_form = forms.UserForm(request.form)
-  if create_form.validate():
+  if request.method == "POST":
     alum = Alumnos(nombre=create_form.nombre.data, apellidos=create_form.apellidos.data, email=create_form.email.data)
     db.session.add(alum)
     db.session.commit()
-  return render_template("index.j2", create_form=create_form)
+  return render_template("index.j2", form=create_form)
+  # return render_template("index.j2")
+
+# @app.post("/")
+# def index_post():
+#   create_form = forms.UserForm(request.form)
+#   if create_form.validate():
+#     alum = Alumnos(nombre=create_form.nombre.data, apellidos=create_form.apellidos.data, email=create_form.email.data)
+#     db.session.add(alum)
+#     db.session.commit()
+#   return render_template("index.j2", form=create_form)
 
 if __name__ == '__main__':
   csrf.init_app(app)
