6e516078c7a5f4f23c6e27f023d45b7bd5fbd716
Juan <juanxxrvillanueva@outlook.com>
2023-03-06 18:47:53 -0600

    ABC parte1 06/03/23

--- app.py
+++ app.py
@@ -20,6 +20,34 @@ def index():
   return render_template("index.j2", form=create_form)
   # return render_template("index.j2")
 
+@app.route('/modificar', methods=["GET", "POST"])
+def modificar():
+  create_form = forms.UserForm(request.form)
+  if request.method == "GET":
+    id=request.args.get("id")
+    alum1 = db.session.query(Alumnos).filter_by(id = id).first()
+    create_form.id.data = request.args.get("id")
+    create_form.nombre.data = alum1.nombre
+    create_form.apellidos.data = alum1.apellidos
+    create_form.email.data = alum1.email
+  if request.method == "POST":
+    id=create_form.id.data
+    alum=db.session.query(Alumnos).filter_by(id=id).first()
+    print(create_form.data)
+    alum.nombre=create_form.nombre.data
+    alum.apellidos=create_form.apellidos.data
+    alum.email=create_form.email.data
+    db.session.add(alum)
+    db.session.commit()
+    return redirect(url_for("ABCompleto"))
+  return render_template("modificar.j2", form=create_form)
+
+@app.route('/ABCompleto', methods=["GET", "POST"])
+def ABCompleto():
+  form = forms.UserForm(request.form);
+  alumnos = db.session.query(Alumnos).all()
+  return render_template("ABCompleto.j2", form = form, alumnos = alumnos)
+
 # @app.post("/")
 # def index_post():
 #   create_form = forms.UserForm(request.form)
